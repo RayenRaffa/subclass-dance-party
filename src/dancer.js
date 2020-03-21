03fd11b2ef2664ea3e80aca7388994da4ed1ce32
raffa <raffa.rayen@gmail.com>
2020-03-21 15:22:19 +0100

    refactor blinkyDancer to pseudo-classical

--- src/dancer.js
+++ src/dancer.js
@@ -1,25 +1,24 @@
 // Creates and returns a new dancer object that can step
-var MakeDancer = function(top, left, timeBetweenSteps) {
+var makeDancer = function(top, left, timeBetweenSteps) {
 
 
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-
+  this.timeBetweenSteps = timeBetweenSteps;
+  // this.step();
   this.step();
   this.setPosition(top, left);
 
+};
+makeDancer.prototype.step = function() {
 
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
-MakeDancer.prototype.step = function() {
-  // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
-};
-MakeDancer.prototype..setPosition = function(top, left) {
+
+makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
-  //
   var styleSettings = {
     top: top,
     left: left
